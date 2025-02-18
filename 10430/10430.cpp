02fd09ff8c19294103b12d60d4f4c828ccd21c46
Mained606 <mained606@gmail.com>
2025-02-18 20:40:10 +0900

    백준 알고리즘 문제 추가 풀이
    3개 추가

---
+++ 10430/10430.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	int num1, num2;
+	std::cin >> num1 >> num2;
+	std::string strNum = std::to_string(num2);
+
+	for (int i = 2; i >= 0; i--)
+	{
+		int digit = strNum[i] - '0';
+		std::cout << num1 * digit << "\n";
+	}
+
+	std::cout << num1 * num2 << "\n";
+
+	return 0;
+}
---
+++ 11382/11382.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main() {
+	long long num1, num2, num3;
+
+	std::cin >> num1 >> num2 >> num3;
+	std::cout << num1 + num2 + num3 << "\n";
+
+	return 0;
+}
