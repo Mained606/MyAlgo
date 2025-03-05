f7a765632661ff8f7eb1d43feff713c72e865942
Mained606 <mained606@gmail.com>
2025-03-05 23:42:44 +0900

    문제 풀이 추가

---
+++ 25304/25304.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int main() {
+	int N = 0;
+	long X = 0;
+	long sum = 0;
+
+	std::cin >> X;
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int a, b;
+		std::cin >> a >> b;
+		sum += a * b;
+	}
+	if (X == sum) {
+		std::cout << "Yes" << "\n";
+	}
+	else {
+		std::cout << "No" << "\n";
+	}
+	return 0;
+}
