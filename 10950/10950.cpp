82a5cad63a456ebf47794a6f6cfe3d10c5e8e6bf
Mained606 <mained606@gmail.com>
2025-03-05 23:33:28 +0900

    문제 풀이 추가

---
+++ 10950/10950.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+
+int main() {
+	int count;
+	int A, B;
+	
+	std::cin >> count;
+	
+	for (int i = 0; i < count; i++) {
+		std::cin >> A >> B;
+		std::cout << A + B << "\n";
+	}
+	return 0;
+}
---
+++ 8393/8393.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+	int n = 0;
+	int result = 0;
+
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		result += i;
+	}
+	std::cout << result << "\n";
+	return 0;
+}
